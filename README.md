1a2d6453f8b1d32c6f095bf157ca9dd3403d9f22
Norman Jarvis <norman.jarvis@evernym.com>
2019-05-22 15:04:12 -0600

    Added react navigation for iOS and Android, added project structure following best practice.

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # finview
 View market prices
 
-rm -rf node_modules ios/Pods ios/build;make clean;make cleancache;yarn install;yarn pod:install;make pre-run;jest;glow
+rm -rf node_modules ios/Pods ios/build;make clean;make cleancache;yarn install;yarn pod:install;make pre-run
 
+npm run test
+
+Android:
+launch Android Studio
+adb devices -l
+adb reverse tcp:8081 tcp:8081
+emulator -writable-system -avd Pixel_XL_API_28 -netdelay none -partition-size 4096 -netspeed full
+npm run start
+run app from Android Studio and choose emulator at startup
+
+
+iOS:
+launch Xcode
+open finview.xcworkspace
+run app in simulator
--- ios/finview/AppDelegate.m
+++ ios/finview/AppDelegate.m
@@ -7,7 +7,7 @@
 
 #import "AppDelegate.h"
 
-#import <React/RCTBridge.h>
+#import <ReactNativeNavigation/ReactNativeNavigation.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
@@ -15,28 +15,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"finview"
-                                            initialProperties:nil];
+  NSURL *jsCodeLocation;
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  UIViewController *rootViewController = [UIViewController new];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
+  [ReactNativeNavigation bootstrap:jsCodeLocation launchOptions:launchOptions];
+
   return YES;
-}
-
-- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-{
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
 }
 
 @end
